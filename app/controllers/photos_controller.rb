16b12c7cef14fdb98a1e2c51a5b60649c88166c6
patrick <khoai.nx@gmail.com>
2017-04-30 21:20:33 +0700

    Change to Posgresql

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -41,7 +41,7 @@ class PhotosController < ApplicationController
   # PATCH/PUT /photos/1
   # PATCH/PUT /photos/1.json
   def update
-    authorize! :manage, @question
+    authorize! :manage, @photo
     respond_to do |format|
       if @photo.update(photo_params)
         format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
@@ -56,7 +56,7 @@ class PhotosController < ApplicationController
   # DELETE /photos/1
   # DELETE /photos/1.json
   def destroy
-    authorize! :manage, @question
+    authorize! :manage, @photo
     @photo.destroy
     respond_to do |format|
       format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
